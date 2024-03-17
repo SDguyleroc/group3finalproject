ff6b20eb2c09433a860c7b31391aecd602563fa9
Guy-leroc Ossebi <thonerguyleroc1@gmail.com>
2024-03-17 09:41:52 -0400

    intitial

---
+++ src/main/java/com/group3finalproject/bloghub/BloghubApplication.java
@@ -0,0 +1,13 @@
+package com.group3finalproject.bloghub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloghubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloghubApplication.class, args);
+	}
+
+}
